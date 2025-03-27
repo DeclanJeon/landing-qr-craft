f64e5cc01a483cb42760aec5af227575f453aaa2
declan <syas0301@gmail.com>
2025-03-27 19:48:15 +0900

    250327 - shoptemplate, shoppage 수정

--- src/pages/ShopPage.tsx
+++ src/pages/ShopPage.tsx
@@ -1,16 +1,20 @@
-
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import ShopTemplate from '@/components/ShopTemplate';
 
 const ShopPage = () => {
-  const { shopUrl, page, categoryId, productId } = useParams();
-  
+  const { shopUrl, productId } = useParams();
+
+  // 현재 경로에서 "about" 또는 "service"를 추출하기 위해 window.location 사용
+  const pathSegments = window.location.pathname.split('/').filter(Boolean);
+  const pageSegment = pathSegments[pathSegments.length - 1];
+  const page = ['about', 'service'].includes(pageSegment) ? pageSegment : undefined;
+
   return (
     <ShopTemplate 
       shopUrl={shopUrl} 
-      page={page} 
-      categoryId={categoryId ? parseInt(categoryId) : undefined}
+      page={page} // "about" 또는 "service", 없으면 undefined
+      categoryId={undefined} // 현재 경로에 categoryId가 없으므로 undefined
       productId={productId}
     />
   );
